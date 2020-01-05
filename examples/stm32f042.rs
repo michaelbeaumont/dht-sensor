f3ce918d40dc1805c4e9aff860568c1cf38ef6e0
Michael Beaumont <mjboamail@gmail.com>
2020-01-05 21:04:03 +0100

    feat: DHT11 support

---
+++ examples/stm32f042.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use crate::hal::{delay, gpio, prelude::*, stm32};
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use panic_halt as _;
+use stm32f0xx_hal as hal;
+
+use dht_sensor::{read11, Reading};
+
+#[entry]
+fn main() -> ! {
+    let mut p = stm32::Peripherals::take().unwrap();
+    let cp = stm32::CorePeripherals::take().unwrap();
+    let mut rcc = p.RCC.configure().sysclk(8.mhz()).freeze(&mut p.FLASH);
+    let mut delay = delay::Delay::new(cp.SYST, &rcc);
+
+    hprintln!("Waiting on the sensor...").unwrap();
+    delay.delay_ms(1000_u16);
+    let gpio::gpioa::Parts { pa1, .. } = p.GPIOA.split(&mut rcc);
+    let mut pa1 = cortex_m::interrupt::free(|cs| pa1.into_open_drain_output(cs));
+    match read11(&mut delay, &mut pa1) {
+        Ok(Reading {
+            temperature,
+            humidity,
+        }) => hprintln!("{}°, {}% RH", temperature, humidity).unwrap(),
+        Err(e) => hprintln!("Error {:?}", e).unwrap(),
+    }
+    hprintln!("Looping forever now, thanks!").unwrap();
+    loop {}
+}
---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+#![no_std]
+
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+
+pub struct Reading {
+    pub temperature: i8,
+    pub humidity: u8,
+}
+
+#[derive(Debug)]
+pub enum DhtError<E> {
+    PinError(E),
+    ChecksumMismatch,
+}
+
+impl<E> From<E> for DhtError<E> {
+    fn from(error: E) -> DhtError<E> {
+        DhtError::PinError(error)
+    }
+}
+
+fn read_bit<D, E>(delay: &mut D, pin: &impl InputPin<Error = E>) -> Result<bool, E>
+where
+    D: DelayUs<u8>,
+    E: core::fmt::Debug,
+{
+    while pin.is_low()? {}
+    delay.delay_us(35u8);
+    let high = pin.is_high()?;
+    while pin.is_high()? {}
+    Ok(high)
+}
+
+fn read_byte<D, E>(delay: &mut D, pin: &impl InputPin<Error = E>) -> Result<u8, E>
+where
+    D: DelayUs<u8>,
+    E: core::fmt::Debug,
+{
+    let mut byte: u8 = 0;
+    for i in 0..8 {
+        let bit_mask = 1 << (7 - (i % 8));
+        if read_bit(delay, pin)? {
+            byte |= bit_mask;
+        }
+    }
+    Ok(byte)
+}
+
+pub fn read11<P, E, D>(delay: &mut D, pin: &mut P) -> Result<Reading, DhtError<E>>
+where
+    P: InputPin<Error = E> + OutputPin<Error = E>,
+    E: core::fmt::Debug,
+    D: DelayMs<u8> + DelayUs<u8>,
+{
+    pin.set_low().ok();
+    delay.delay_ms(18_u8);
+    pin.set_high().ok();
+    delay.delay_us(48_u8);
+    while pin.is_low()? {}
+    while pin.is_high()? {}
+    let humidity = read_byte(delay, pin)?;
+    let _ = read_byte(delay, pin)?;
+    let temperature = read_byte(delay, pin)?;
+    let _ = read_byte(delay, pin)?;
+    let checksum = read_byte(delay, pin)?;
+    if humidity + temperature != checksum {
+        Err(DhtError::ChecksumMismatch)
+    } else {
+        Ok(Reading {
+            temperature: temperature as i8,
+            humidity,
+        })
+    }
+}
