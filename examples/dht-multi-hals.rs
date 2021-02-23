e687696f53f6b9aba8067a6ffe551c4fd0661552
Paul Gilbert <pdgilbert@users.noreply.github.com>
2021-02-22 20:43:01 -0500

    Adjusted comments to indicate need for --release when running the code examples. Added comments about run tests.

--- examples/dht-multi-hals.rs
+++ examples/dht-multi-hals.rs
@@ -5,21 +5,30 @@
 //!  The largest part of this file is the setup() functions used for each hal. 
 //!  These make the application code common.
 
-//!  This examples has been tested to run with ...
+//!  This examples has been tested to run with both DHT11 and DHT22 on bluepill (stm32f103), 
+//!  blackpill-stm32f401, and  blackpill-stm32f411. As of Feb. 2021 there are known
+//!  runtime error on
+//!  discovery-stm32f303 reads both DHT11 and DHT22  once but then fails at delay.
+//!  discovery-stm32l100 gives Error Timeout with both DHT11 and DHT22. 
+//!
 //!  In linux with environment variables as below the example can be compiled, loaded and 
 //!  run using gdb and openocd
 //!  	openocd -f interface/$INTERFACE.cfg -f target/$PROC.cfg 
 //!  In another window do
-//!  	cargo  run --target $TARGET --features $HAL,$MCU --example dht-multi-hals
+//!  	cargo  run --target $TARGET --features $HAL,$MCU    --example dht-multi-hals --release
 //!  or if a DHT22 sensor is used
-//!  	cargo  run --target $TARGET --features $HAL,$MCU,dht22 --example dht-multi-hals
+//!  	cargo  run --target $TARGET --features $HAL,$MCU,dht22 --example dht-multi-hals --release
 //!  
 //!  To only compile use
-//!  	cargo  build --target $TARGET --features $HAL,$MCU --example dht-multi-hals
+//!  	cargo  build --target $TARGET --features $HAL,$MCU --example dht-multi-hals [ --release ]
 //!  or 
-//!  	cargo  build --target $TARGET --features $HAL,$MCU,dht22 --example dht-multi-hals
+//!  	cargo  build --target $TARGET --features $HAL,$MCU,dht22 --example dht-multi-hals  [ --release ]
 //!
-//!  In the case of an error  '.rodata will not fit in region FLASH ' add ' --release' to the command.
+//!  If --release is omitted then some MCUs do not have  sufficient memory an loading results in
+//!       '.rodata will not fit in region FLASH ' 
+//!
+//!  However, even with sufficient memory the code without --release is slower and often results in an
+//!  'Error Timeout' so it is recommended to use --release when attempting to run the code.
 //!
 //!                cargo run  environment variables                        openocd        test board and processor
 //!    _____________________________________________________________     _____________   ___________________________
@@ -35,14 +44,17 @@
 //!    hal NOT compiling as of (Feb 2021) export HAL=stm32f7xx MCU=stm32f722 TARGET=thumbv7em-none-eabihf #none-stm32f722 Cortex-M7
 //!    export HAL=stm32h7xx MCU=stm32h742   TARGET=thumbv7em-none-eabihf                # none-stm32h742      Cortex-M7
 //!    export HAL=stm32l0xx MCU=stm32l0x2   TARGET=thumbv6m-none-eabi	 PROC=stm32l1   # none-stm32l0x2      Cortex-M0
-//!    export HAL=stm32l1xx MCU=stm32l100   TARGET=thumbv7m-none-eabi	                # discovery-stm32l100 Cortex-M3
+//!    export HAL=stm32l1xx MCU=stm32l100   TARGET=thumbv7m-none-eabi	 PROC=stm32l1   # discovery-stm32l100 Cortex-M3
 //!    export HAL=stm32l1xx MCU=stm32l151   TARGET=thumbv7m-none-eabi	 PROC=stm32l1   # heltec-lora-node151 Cortex-M3
-//!    NOT compiling as of (Feb 2021) export HAL=stm32l4xx MCU=stm32l4x2   TARGET=thumbv7em-none-eabi                  # none-stm32l4x1      Cortex-M4
+//!    NOT compiling as of (Feb 2021) export HAL=stm32l4xx MCU=stm32l4x2   TARGET=thumbv7em-none-eabi # none-stm32l4x1      Cortex-M4
 //!  
 //!  Depending on the MCU connection to the computer, in the  openocd command use
 //!    export INTERFACE=stlink-v2  
 //!    export INTERFACE=stlink-v2-1  
 
+//! A version of this example is reported at https://pdgilbert.github.io/eg_stm_hal/. 
+//! The results reported there use current git versions of the hals, whereas 
+//! the testing reported above uses release versions of the hals (as of Feb 2021).
 
 #![deny(unsafe_code)]
 #![no_main]
